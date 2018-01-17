72bdaf7c04c469b09174b9d94c64f1b058a1fcd9
Hoo <zhanghoo@163.com>
2018-01-17 10:03:20 +0800

    Update chat.js

--- js/chat.js
+++ js/chat.js
@@ -1,3 +1,14 @@
 $(function() {
-  
+  var mySwiper = new Swiper('.swiper-container',{
+    direction: 'vertical',
+    freeMode: true,
+    autoHeight: true,
+    roundLengths: true,
+    observer: true,
+    observeParents: true,
+    scrollbar: {
+      el: '.swiper-scrollbar',
+      hide: true,
+    },
+  });
 })
